0bdecbb820a0f628427ce8fa6508b6129e7c6ee6
tunmie <victoroluwasomidotun@gmail.com>
2023-01-24 20:06:59 +0100

    server found

---
+++ src/lib/data/todoData.js
@@ -0,0 +1,35 @@
+let todos = [
+	{ id: 1, title: "Write SvelteKit intro blog post", assigned: "Adam", tags: [1] },
+	{ id: 2, title: "Write SvelteKit advanced data loading blog post", assigned: "Adam", tags: [1] },
+	{ id: 3, title: "Prepare RenderATL talk", assigned: "Adam", tags: [2] },
+	{ id: 4, title: "Fix all SvelteKit bugs", assigned: "Rich", tags: [3] },
+	{ id: 5, title: "Edit Adam's blog posts", assigned: "Geoff", tags: [4] },
+]
+
+let tags = [
+	{ id: 1, name: "SvelteKit Content", color: "ded" },
+	{ id: 2, name: "Conferences", color: "purple" },
+	{ id: 3, name: "SvelteKit Development", color: "pink" },
+	{ id: 4, name: "CSS-Tricks Admin", color: "blue" },
+]
+
+export const wait = async amount => new Promise(res => setTimeout(res, amount ?? 100));
+
+export async function getTodos() {
+	await wait();
+
+	return todos;
+}
+
+export async function getTags() {
+	await wait();
+
+	return tags.reduce((lookup, tag) => {
+		lookup[tag.id] = tag;
+		return lookup;
+	}, {})
+}
+
+export async function getTodo(id) {
+	return todos.find(t => t.id === id);
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+	import './styles.css'
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/list">Todo List</a>
+		</li>
+		<li>
+			<a href="/admin/paid-status">Account Status</a>
+		</li>
+		<li>
+			<a href="/admin/user-settings">User Settings</a>
+		</li>
+	</ul>
+</nav>
+
+<slot/>
+
+<style>
+	nav {
+			background-color: beige;
+	}
+
+	nav ul {
+			display: flex;
+	}
+
+	li {
+			list-style: none;
+      margin: 15px;
+	}
+	
+	a {
+      text-decoration: none;
+			color: black;
+	}
+</style>
