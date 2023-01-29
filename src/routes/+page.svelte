3196b712b84bd770db244dd30349e35a62c9ad16
tunmie <victoroluwasomidotun@gmail.com>
2023-01-29 07:07:44 +0100

    layout in progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,42 @@
-<h1>Root Page</h1>
+<body>
+  <section class="section-1">
+    <div class="intro-text">
+      <p>Task <br> Management <br> Made <span>Easy.</span></p>
+      <button>Get Started</button>
+    </div>
+    <div class="intro-img"></div>
+  </section>
+  <section class="section-2">
+    <div class="intro-text"></div>
+    <div class="intro-img"></div>
+  </section>
+</body>
+
+<style>
+  body {
+    background-color: #fff;
+  }
+  .section-1 {
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+
+  .section-2 {
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+
+  .intro-text {
+    width: 50%;
+    height: 100%;
+    border: 2px solid black;
+  }
+
+  .intro-img {
+    width: 50%;
+    height: 100%;
+    border: 2px solid black;
+  }
+</style>
