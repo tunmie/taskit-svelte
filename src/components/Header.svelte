392648667d9f65cfecd601629581cffd92730899
tunmie <victoroluwasomidotun@gmail.com>
2023-01-29 06:22:40 +0100

    navigation done

---
+++ src/components/Header.svelte
@@ -0,0 +1,124 @@
+<script>
+	import logo from '$lib/assets/logo.png';
+</script>
+
+
+<header>
+	<div class='logo'>
+		<a href='/'>
+			<img src={logo} alt='our logo' />
+			<h1>List Things</h1>
+		</a>
+	</div>
+
+
+	<nav>
+		<ul>
+			<li><a href='/'>Why Us?</a></li>
+		</ul>
+		<ul>
+			<li><a href='/'>Contact</a></li>
+		</ul>
+		<ul>
+			<li><a href='/'>Features</a></li>
+		</ul>
+	</nav>
+	<div class="auth-container">
+		<button >Sign Up</button>
+		<button class="login-btn">Log In</button>
+	</div>
+
+</header>
+
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		background-color: #F0FFF0;
+	}
+
+	ul {
+		position: relative;
+		list-style: none;
+		margin: 0;
+		padding: 3em;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	ul li {
+		height: 100%;
+	}
+
+	ul li a {
+		font-size: 18px;
+		font-weight: 600;
+		line-height: 100%;
+		letter-spacing: -0.02em;
+		color: #020130;
+		font-family: "Poppins", sans-serif;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	.logo {
+		/* width: 3em; */
+		/* height: 3em; */
+		margin-left: 88px;
+		margin-top: 20px;
+		margin-bottom: 20px;
+	}
+
+	.logo a {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+	}
+
+	.logo a h1 {
+		font-family: "Poppins", sans-serif;
+		font-size: 20px;
+		font-weight: 700;
+		letter-spacing: -0.02em;
+		color: #020130;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	.auth-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+    margin-right: 40px;
+	}
+
+
+	button {
+		background-color: #020130;
+		color: #fff;
+		padding: 10px 25px;
+    margin: 18px;
+		border-radius: 12px;
+		border: none;
+		font-size: 18px;
+    letter-spacing: -0.02em;
+	}
+
+  .login-btn {
+    background: none;
+    color: #020130;
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+  }
+
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,10 @@
 <script>
-	import '../app.css'
-	import logo from "$lib/assets/logo.png"
+	import Header from '../components/Header.svelte';
+	import './styles.css'
 </script>
 
-<nav class='container mx-auto'>
-	<ul class='flex flex-row justify-around'>
-		<li>
-			<div class="logo">
-				<img src={logo} alt="our logo" class="px-">
-				<a href="/">List Things</a>
-			</div>
-		</li>
-		<li>
-			<a class="nav-item">Why Us?</a>
-		</li>
-		<li>
-			<a>Contact</a>
-		</li>
-		<li>
-			<a>Features</a>
-		</li>
-	</ul>
-</nav>
+<Header />
 
 <slot/>
 
-<style>
-	nav {
-		background-color: #F0FFF0;
-	}
-
-</style>
 
